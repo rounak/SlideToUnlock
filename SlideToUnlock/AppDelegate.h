7638bb92562132e17cbacbaa9225701b01979bea
rounak <rounak91@gmail.com>
2012-09-03 02:23:24 +0530

    Initial Commit

---
+++ SlideToUnlock/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  SlideToUnlock
+//
+//  Created by Rounak Jain on 03/09/12.
+//  Copyright (c) 2012 Rounak Jain. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ SlideToUnlock/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  SlideToUnlock
+//
+//  Created by Rounak Jain on 03/09/12.
+//  Copyright (c) 2012 Rounak Jain. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
